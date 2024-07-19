49e17666056db86dc269a263536aff46c129c9b3
emmanuel-mutua <emmamulwa2020@gmail.com>
2024-07-19 13:21:19 +0300

    add farmer page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Sidebar from './sidebar/Sidebar';
+import Farmers from './farmer/Farmer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Sidebar />
+      <div className="content">
+        <header>
+          <input type="text" placeholder="Search" className="search-bar"/>
+          <div className="user-info">
+            <span>Mabroukie Factory</span>
+            <span>mabroukie@gmail.com</span>
+          </div>
+        </header>
+        <Farmers />
+      </div>
     </div>
   );
 }
